577d2a2cbfed28e24abb7b5e5d96418990852860
ThanhBT <105102395+kjjjoP3@users.noreply.github.com>
2024-11-10 13:47:11 +0700

    a

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,34 +2,34 @@
 #include <iostream>
 #include <string>
 
-Renderer::Renderer(const std::size_t screenWidth,
-                   const std::size_t screenHeight,
-                   const std::size_t gridWidth, const std::size_t gridHeight)
-    : screen_width(screenWidth),
-      screen_height(screenHeight),
-      grid_width(gridWidth),
-      grid_height(gridHeight) {
-  // Khởi tạo SDL
+Renderer::Renderer(const std::size_t screen_width,
+                   const std::size_t screen_height,
+                   const std::size_t grid_width, const std::size_t grid_height)
+    : screen_width(screen_width),
+      screen_height(screen_height),
+      grid_width(grid_width),
+      grid_height(grid_height) {
+  // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    std::cerr << "SDL không thể khởi tạo.\n";
-    std::cerr << "Lỗi SDL: " << SDL_GetError() << "\n";
+    std::cerr << "SDL could not initialize.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
 
-  // Tạo cửa sổ
+  // Create Window
   sdl_window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
                                 SDL_WINDOWPOS_CENTERED, screen_width,
                                 screen_height, SDL_WINDOW_SHOWN);
 
   if (nullptr == sdl_window) {
-    std::cerr << "Cửa sổ không thể được tạo.\n";
-    std::cerr << "Lỗi SDL: " << SDL_GetError() << "\n";
+    std::cerr << "Window could not be created.\n";
+    std::cerr << " SDL_Error: " << SDL_GetError() << "\n";
   }
 
-  // Tạo renderer
+  // Create renderer
   sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
   if (nullptr == sdl_renderer) {
-    std::cerr << "Không thể tạo renderer.\n";
-    std::cerr << "Lỗi SDL: " << SDL_GetError() << "\n";
+    std::cerr << "Renderer could not be created.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
 }
 
@@ -38,119 +38,102 @@ Renderer::~Renderer() {
   SDL_Quit();
 }
 
-void Renderer::Render(const Snake& snake, const SDL_Point& food) {
+void Renderer::Render(Snake const& snake, SDL_Point const& food) {
   SDL_Rect block;
   block.w = screen_width / grid_width;
   block.h = screen_height / grid_height;
 
-  // Xóa màn hình
+  // Clear screen
   SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
   SDL_RenderClear(sdl_renderer);
 
-  // Vẽ thức ăn
+  // Render food
   SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
   SDL_RenderFillRect(sdl_renderer, &block);
 
-  // Vẽ thân rắn
+  // Render snake's body
   RenderBody(snake, block);
 
-  // Vẽ đầu rắn
+  // Render snake's head
   block.x = static_cast<int>(snake.GetHead().x) * block.w;
   block.y = static_cast<int>(snake.GetHead().y) * block.h;
   if (snake.IsAlive()) {
-    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF); // Màu xanh cho đầu rắn sống
+    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
   } else {
-    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF); // Màu đỏ cho đầu rắn chết
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
   }
   SDL_RenderFillRect(sdl_renderer, &block);
 
-  // Cập nhật màn hình
+  // Update Screen
   SDL_RenderPresent(sdl_renderer);
 }
 
 void Renderer::UpdateWindowTitle(int score, int fps) {
-  std::string title = "Snake Score: " + std::to_string(score) + " FPS: " + std::to_string(fps);
+  std::string title{"Snake Score: " + std::to_string(score) + " FPS: " + std::to_string(fps)};
   SDL_SetWindowTitle(sdl_window, title.c_str());
 }
 
-Renderer::Direction Renderer::Oriented(int x1, int y1, int x2, int y2) {
-  // x1, y1 là điểm tiền nhiệm, x2, y2 là điểm đang nhìn
-  if (y1 < y2) {
-    return Direction::kUp;
-  } else if (y1 > y2) {
-    return Direction::kDown;
-  } else if (x1 < x2) {
-    return Direction::kLeft;
-  } else {
+Renderer::Direction Renderer::GetDirectionFromPoints(int x1, int y1, int x2, int y2) {
+    if (y1 < y2) return Direction::kUp;
+    if (y1 > y2) return Direction::kDown;
+    if (x1 < x2) return Direction::kLeft;
     return Direction::kRight;
-  }
-  return Direction::kDown; // không bao giờ đến đây
 }
 
-Renderer::Direction Renderer::Oriented(SDL_Point p1, SDL_Point p2) {
-  // p1 là điểm tiền nhiệm
-  if (p1.y < p2.y) {
-    return Direction::kUp;
-  } else if (p1.y > p2.y) {
-    return Direction::kDown;
-  } else if (p1.x < p2.x) {
-    return Direction::kLeft;
-  } else {
+Renderer::Direction Renderer::GetDirectionFromPoints(SDL_Point p1, SDL_Point p2) {
+    if (p1.y < p2.y) return Direction::kUp;
+    if (p1.y > p2.y) return Direction::kDown;
+    if (p1.x < p2.x) return Direction::kLeft;
     return Direction::kRight;
-  }
-  return Direction::kDown; // không bao giờ đến đây
 }
 
-void Renderer::RenderBlock(Direction dir, int x, int y, SDL_Rect& block) {
-  // Vẽ các khối với hướng tương ứng
-  switch (dir) {
+void Renderer::RenderSingleBlock(Direction dir, int x, int y, SDL_Rect& block) {
+    switch (dir) {
     case Direction::kUp:
-      block.x = x * block.w + 1;
-      block.y = y * block.h - 1;
-      block.w -= 2;
-      SDL_RenderFillRect(sdl_renderer, &block);
-      block.w += 2;
-      break;
+        block.x = x * block.w + 1;
+        block.y = y * block.h - 1;
+        block.w -= 2;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.w += 2;
+        break;
     case Direction::kDown:
-      block.x = x * block.w + 1;
-      block.y = y * block.h + 1;
-      block.w -= 2;
-      SDL_RenderFillRect(sdl_renderer, &block);
-      block.w += 2;
-      break;
+        block.x = x * block.w + 1;
+        block.y = y * block.h + 1;
+        block.w -= 2;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.w += 2;
+        break;
     case Direction::kLeft:
-      block.x = x * block.w - 2;
-      block.y = y * block.h + 1;
-      block.h -= 2;
-      block.w++;
-      SDL_RenderFillRect(sdl_renderer, &block);
-      block.h += 2;
-      block.w--;
-      break;
+        block.x = x * block.w - 2;
+        block.y = y * block.h + 1;
+        block.h -= 2;
+        block.w++;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.h += 2;
+        block.w--;
+        break;
     case Direction::kRight:
-      block.x = x * block.w + 1;
-      block.y = y * block.h + 1;
-      block.h -= 2;
-      SDL_RenderFillRect(sdl_renderer, &block);
-      block.h += 2;
-      break;
-  }
+        block.x = x * block.w + 1;
+        block.y = y * block.h + 1;
+        block.h -= 2;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.h += 2;
+        break;
+    }
 }
 
-void Renderer::RenderBody(const Snake& snake, SDL_Rect& block) {
-  Direction orientation;
-  const std::vector<SDL_Point>& body = snake.GetBody();
-  int x = static_cast<int>(snake.GetHead().x);
-  int y = static_cast<int>(snake.GetHead().y);
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF); // Màu trắng cho thân rắn
-  orientation = Oriented(x, y, body.back().x, body.back().y);
-  RenderBlock(orientation, body.back().x, body.back().y, block);
-  
-  // Duyệt qua toàn bộ thân rắn và vẽ
-  for (auto point = body.rbegin() + 1; point != body.rend(); ++point) {
-    orientation = Oriented(*(point - 1), *point);
-    RenderBlock(orientation, point->x, point->y, block);
-  }
+void Renderer::RenderBody(Snake const& snake, SDL_Rect& block) {
+    Direction orientation;
+    const std::vector<SDL_Point>& body = snake.GetBody();
+    int x = static_cast<int>(snake.GetHead().x);
+    int y = static_cast<int>(snake.GetHead().y);
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    orientation = GetDirectionFromPoints(x, y, body.back().x, body.back().y);
+    RenderSingleBlock(orientation, body.back().x, body.back().y, block);
+    for (auto point = body.rbegin() + 1; point != body.rend(); point++) {
+        orientation = GetDirectionFromPoints(*(point - 1), *point);
+        RenderSingleBlock(orientation, point->x, point->y, block);
+    }
 }

0345d9dada08cd160b3aad6eec51db80c556cb23
ThanhBT <105102395+kjjjoP3@users.noreply.github.com>
2024-11-13 22:18:55 +0700

    update

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,26 +9,26 @@ Renderer::Renderer(const std::size_t screen_width,
       screen_height(screen_height),
       grid_width(grid_width),
       grid_height(grid_height) {
-  // Khởi tạo SDL
+  // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    std::cerr << "Không thể khởi tạo SDL.\n";
+    std::cerr << "SDL could not initialize.\n";
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
 
-  // Tạo cửa sổ
+  // Create Window
   sdl_window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
                                 SDL_WINDOWPOS_CENTERED, screen_width,
                                 screen_height, SDL_WINDOW_SHOWN);
 
-  if (!sdl_window) {
-    std::cerr << "Không thể tạo cửa sổ.\n";
-    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+  if (nullptr == sdl_window) {
+    std::cerr << "Window could not be created.\n";
+    std::cerr << " SDL_Error: " << SDL_GetError() << "\n";
   }
 
-  // Tạo renderer
+  // Create renderer
   sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
-  if (!sdl_renderer) {
-    std::cerr << "Không thể tạo renderer.\n";
+  if (nullptr == sdl_renderer) {
+    std::cerr << "Renderer could not be created.\n";
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
 }
@@ -38,29 +38,35 @@ Renderer::~Renderer() {
   SDL_Quit();
 }
 
-void Renderer::Render(const Snake &snake, const SDL_Point &food) {
-  SDL_Rect block{.w = screen_width / grid_width, .h = screen_height / grid_height};
+void Renderer::Render(Snake const snake, SDL_Point const &food) {
+  SDL_Rect block;
+  block.w = screen_width / grid_width;
+  block.h = screen_height / grid_height;
 
-  // Xóa màn hình
+  // Clear screen
   SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
   SDL_RenderClear(sdl_renderer);
 
-  // Vẽ thức ăn
+  // Render food
   SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
   SDL_RenderFillRect(sdl_renderer, &block);
 
-  // Vẽ thân rắn
+  // Render snake's body
   RenderBody(snake, block);
 
-  // Vẽ đầu rắn
+  // Render snake's head
   block.x = static_cast<int>(snake.GetHead().x) * block.w;
   block.y = static_cast<int>(snake.GetHead().y) * block.h;
-  SDL_SetRenderDrawColor(sdl_renderer, snake.IsAlive() ? 0x00 : 0xFF, snake.IsAlive() ? 0x7A : 0x00, snake.IsAlive() ? 0xCC : 0x00, 0xFF);
+  if (snake.IsAlive() ) {
+    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
+  } else {
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
+  }
   SDL_RenderFillRect(sdl_renderer, &block);
 
-  // Cập nhật màn hình
+  // Update Screen
   SDL_RenderPresent(sdl_renderer);
 }
 
@@ -69,48 +75,89 @@ void Renderer::UpdateWindowTitle(int score, int fps) {
   SDL_SetWindowTitle(sdl_window, title.c_str());
 }
 
-Renderer::Direction Renderer::Oriented(int x1, int y1, int x2, int y2) {
-    if (y1 != y2) return (y1 < y2) ? Direction::kUp : Direction::kDown;
-    return (x1 < x2) ? Direction::kLeft : Direction::kRight;
+Renderer::Direction Renderer::Oriented(int x1, int y1, int x2, int y2)
+{
+    // x1,y1 are predecessor - what x2,y2 are looking at..
+    if (y1 < y2)
+        return Direction::kUp;
+    else
+        if (y1 > y2)
+            return Direction::kDown;
+        else
+            if (x1 < x2)
+                return Direction::kLeft;
+            else
+                return Direction::kRight;
+    return Direction::kDown; // should never reach here..
 }
 
-Renderer::Direction Renderer::Oriented(const SDL_Point &p1, const SDL_Point &p2) {
-    return Oriented(p1.x, p1.y, p2.x, p2.y);
+Renderer::Direction Renderer::Oriented(SDL_Point p1, SDL_Point p2)
+{   // p1 is predecessor
+    if (p1.y < p2.y)
+        return Direction::kUp;
+    else
+        if (p1.y > p2.y)
+            return Direction::kDown;
+        else
+            if (p1.x < p2.x)
+                return Direction::kLeft;
+            else
+                return Direction::kRight;
+    return Direction::kDown; // should never reach here..
+
 }
 
-void Renderer::RenderBlock(Direction dir, int x, int y, SDL_Rect &block) {
-  auto configureBlock = [&](int offsetX, int offsetY, int widthAdjustment, int heightAdjustment) {
-    block.x = x * block.w + offsetX;
-    block.y = y * block.h + offsetY;
-    block.w += widthAdjustment;
-    block.h += heightAdjustment;
-    SDL_RenderFillRect(sdl_renderer, &block);
-  };
+void Renderer::RenderBlock(Direction dir, int x, int y, SDL_Rect& block)
+{   // draw the appropriate rectangle with (sadly) in-place manipulation of block, which
+    // should be left as it was, in the end (in terms of w and h, not x,y )
+    switch (dir) {
+    case Direction::kUp :
+        block.x = x * block.w + 1;
+        block.y = y * block.h - 1;
+        block.w -= 2;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.w += 2;
+        break;
+    case Direction::kDown :
+        block.x = x * block.w + 1;
+        block.y = y * block.h + 1;
+        block.w -= 2;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.w += 2;
+        break;
+    case Direction::kLeft :
+        block.x = x * block.w - 2;
+        block.y = y * block.h + 1;
+        block.h -= 2;
+        block.w++;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.h += 2;
+        block.w--;
+        break;
+    case Direction::kRight :
+        block.x = x * block.w + 1;
+        block.y = y * block.h + 1;
+        block.h -= 2;
+        //block.w++;
+        SDL_RenderFillRect(sdl_renderer, &block);
+        block.h += 2;
+        //block.w--;
+        break;
 
-  switch (dir) {
-    case Direction::kUp:
-      configureBlock(1, -1, -2, 0);
-      break;
-    case Direction::kDown:
-      configureBlock(1, 1, -2, 0);
-      break;
-    case Direction::kLeft:
-      configureBlock(-2, 1, 1, -2);
-      break;
-    case Direction::kRight:
-      configureBlock(1, 1, 0, -2);
-      break;
-  }
+    }
 }
 
-void Renderer::RenderBody(const Snake &snake, SDL_Rect &block) {
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-  const auto &body = snake.GetBody();
-  auto orientation = Oriented(snake.GetHead().x, snake.GetHead().y, body.back().x, body.back().y);
-  
-  RenderBlock(orientation, body.back().x, body.back().y, block);
-  for (auto it = body.rbegin() + 1; it != body.rend(); ++it) {
-    orientation = Oriented(*(it - 1), *it);
-    RenderBlock(orientation, it->x, it->y, block);
-  }
+void Renderer::RenderBody(Snake const snake, SDL_Rect &block)
+{
+    Direction orientation;
+    const std::vector<SDL_Point>& body = snake.GetBody();
+    int x = static_cast<int>(snake.GetHead().x);
+    int y = static_cast<int>(snake.GetHead().y);
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    orientation = Oriented(x, y, body.back().x, body.back().y  );
+    RenderBlock(orientation, body.back().x, body.back().y, block);
+    for (auto point = body.rbegin() +1; point != body.rend(); point++) {
+        orientation = Oriented( *(point - 1), *point);
+        RenderBlock(orientation, point->x, point->y, block);
+    }
 }
--- src/renderer.h
+++ src/renderer.h
@@ -7,38 +7,27 @@
 
 class Renderer {
  public:
-  // Hướng di chuyển của các khối trong trò chơi
-  enum class Direction { kUp, kDown, kLeft, kRight };
+     enum class Direction { kUp, kDown, kLeft, kRight };
 
-  // Constructor & Destructor
   Renderer(const std::size_t screen_width, const std::size_t screen_height,
            const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
 
-  // Hàm để render trò chơi
-  void Render(const Snake& snake, const SDL_Point& food);
-
-  // Render toàn bộ thân của rắn
-  void RenderBody(const Snake& snake, SDL_Rect& block);
-
-  // Cập nhật tiêu đề cửa sổ với điểm số và FPS
+  void Render(Snake const snake, SDL_Point const &food);
+  void RenderBody(Snake const snake, SDL_Rect &block);
   void UpdateWindowTitle(int score, int fps);
-
-  // Xác định hướng dựa trên tọa độ hai điểm đầu vào
-  Direction GetDirectionFromPoints(int x1, int y1, int x2, int y2);
-  Direction GetDirectionFromPoints(SDL_Point p1, SDL_Point p2);
-
-  // Render một khối đơn lẻ với hướng và tọa độ cho trước
-  void RenderSingleBlock(Direction dir, int x, int y, SDL_Rect& block);
+  Direction Oriented(int x1, int y1, int x2, int y2);
+  Direction Oriented(SDL_Point, SDL_Point);
+  void RenderBlock(Direction, int, int, SDL_Rect& block);
 
  private:
-  SDL_Window* sdl_window;    // Con trỏ tới cửa sổ SDL
-  SDL_Renderer* sdl_renderer; // Con trỏ tới renderer của SDL
+  SDL_Window *sdl_window;
+  SDL_Renderer *sdl_renderer;
 
-  const std::size_t screen_width;  // Chiều rộng màn hình
-  const std::size_t screen_height; // Chiều cao màn hình
-  const std::size_t grid_width;    // Chiều rộng của lưới
-  const std::size_t grid_height;   // Chiều cao của lưới
+  const std::size_t screen_width;
+  const std::size_t screen_height;
+  const std::size_t grid_width;
+  const std::size_t grid_height;
 };
 
 #endif
